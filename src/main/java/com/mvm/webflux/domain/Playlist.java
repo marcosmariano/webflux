b4f9a273ff26a21a1d41270a80ea50e5456a20f4
Marcos Vinicius Mariano <marcos_viniciussilva@hotmail.com>
2022-02-26 02:05:09 -0300

    refactor: add created at

--- src/main/java/com/mvm/webflux/domain/Playlist.java
+++ src/main/java/com/mvm/webflux/domain/Playlist.java
@@ -3,15 +3,27 @@ package com.mvm.webflux.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Document
 public class Playlist {
     @Id
     private String id;
     private String nome;
+    private Date createdAt;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = new Date();
+    }
 
     public Playlist(String id, String nome) {
         this.id = id;
         this.nome = nome;
+        this.createdAt = new Date();
     }
 
     public String getId() {
--- src/main/java/com/mvm/webflux/infra/data/DummyData.java
+++ src/main/java/com/mvm/webflux/infra/data/DummyData.java
@@ -4,6 +4,7 @@ import com.mvm.webflux.domain.Playlist;
 import com.mvm.webflux.infra.repository.PlaylistRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 import java.util.UUID;
@@ -20,18 +21,18 @@ public class DummyData implements CommandLineRunner {
     public void run(String... args) throws Exception {
         this.playlistRepository.deleteAll()
                 .thenMany(
-                        Flux.just("Cleyton e Camargo - Na hora de amar",
-                                "Zeze de Camargo e Luciano - A Lua",
-                                "Bruno e Marrone - Por um minuto",
-                                "Cesar Menotti e Fabiano - Anjo",
-                                "Wesley Safadao - Virote",
-                                "Rionegro e Solimoes - Na sola da bota",
-                                "Gusttavo Lima - Homem de Familia",
-                                "Leonardo - Goiania",
-                                "Jorge e Matheus - Nao para de chover",
-                                "Jorge e Mathes - Ai ja era")
+                        Flux.just("Bruno e Marrone - Por um minuto",
+                                        "Cesar Menotti e Fabiano - Anjo",
+                                        "Cleyton e Camargo - Na hora de amar",
+                                        "Gusttavo Lima - Homem de Familia",
+                                        "Jorge e Mathes - Ai ja era",
+                                        "Jorge e Matheus - Nao para de chover",
+                                        "Leonardo - Goiania",
+                                        "Rionegro e Solimoes - Na sola da bota",
+                                        "Wesley Safadao - Virote",
+                                        "Zeze de Camargo e Luciano - A Lua")
                                 .map(name -> new Playlist(UUID.randomUUID().toString(), name))
                                 .flatMap(playlistRepository::save))
-                                .subscribe(System.out::println);
+                .subscribe(System.out::println);
     }
 }
