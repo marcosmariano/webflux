24c462c8af42785e285cd481f4ac56884146ce21
Marcos Vinicius Mariano <marcos_viniciussilva@hotmail.com>
2022-03-02 04:03:23 -0300

    feature: add cache

--- src/main/java/com/mvm/webflux/api/PlaylistRouter.java
+++ src/main/java/com/mvm/webflux/api/PlaylistRouter.java
@@ -3,10 +3,8 @@ package com.mvm.webflux.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
